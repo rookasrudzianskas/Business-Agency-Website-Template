86d5368bc76a50393e6884dc57d84a2caac24dfd
Rokas <rookas.rudzenskas@gmail.com>
2021-05-20 18:10:48 +0300

    Rokas

---
+++ Starter Files/script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+})
