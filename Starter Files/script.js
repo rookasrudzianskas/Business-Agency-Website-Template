c647e772a135d8c76b90bb07b2426d45b42518b9
Rokas <rookas.rudzenskas@gmail.com>
2021-05-23 15:37:06 +0300

    Rokas

--- Starter Files/script.js
+++ Starter Files/script.js
@@ -7,3 +7,21 @@ menu.addEventListener('click', () => {
     menu.classList.toggle("change");
 })
 
+// section 2 video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+    if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    } else {
+        video.pause()
+        btn.className = 'far fa-play-circle'
+    }
+}
+
+btn.addEventListener('click', () => {
+    playPause()
+})
