f20ca59ab4e6c35dd9293183768c87041d53ec5e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-27 18:33:37 +0300

    Rokas

--- README.md
+++
@@ -1,2 +0,0 @@
-# Business-Agency-Website-Template
-This is business owners, website template, feel free to use it and adjust to your needs
--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const menu = document.querySelector('.menu');
-const navbar = document.querySelector('.navbar');
+// Navbar 
+const menu = document.querySelector('.menu')
+const navbar = document.querySelector('.navbar')
 
 menu.addEventListener('click', () => {
-    navbar.classList.toggle('change');
-    console.log("DONE")
-    menu.classList.toggle("change");
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
 })
+// End of Navbar 
 
-// section 2 video
+// Section 2 Video
 const video = document.querySelector('.video')
 const btn = document.querySelector('.buttons i')
 const bar = document.querySelector('.video-bar')
 
 const playPause = () => {
     if(video.paused) {
-    video.play()
-    btn.className = 'far fa-pause-circle'
-        video.style.opacity = '0.7'
+        video.play()
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = '.7'
     } else {
         video.pause()
         btn.className = 'far fa-play-circle'
-        video.style.opacity = '0.3'
+        video.style.opacity = '.3'
     }
 }
 
@@ -29,26 +30,27 @@ btn.addEventListener('click', () => {
 })
 
 video.addEventListener('timeupdate', () => {
-    console.log(video.currentTime, video.duration)
     const barWidth = video.currentTime / video.duration
     bar.style.width = `${barWidth * 100}%`
     if(video.ended) {
         btn.className = 'far fa-play-circle'
-        video.style.opacity = '0.3'
+        video.style.opacity = '.3'
     }
 })
+// End of Section 2 Video
 
-
-let swiper = new Swiper('.swiper-container', {
+// Section 3 Pricing Cards
+var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 70,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+      rotate: 70,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
-});
+  });
+// End of Section 3 Pricing Cards
